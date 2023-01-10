630e434821f2586849c99f6bce7f1a07dc93f075
bryer01 <97483747+bryer01@users.noreply.github.com>
2023-01-10 14:43:49 -0700

    new changes from class

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,10 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {Random randomgenerator = new Random();
+      Console.WriteLine("Guess a Random Number"  );
     }
+
+
 }
+
---
+++ practice/practice1.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine(Addnumbers(2,2));}
+    static int AddNumbers(int num1, int num2){
+        return num1 + num2;
+
+    }
+}
