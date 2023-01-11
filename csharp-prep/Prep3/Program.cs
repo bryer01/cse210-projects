03ff047f35a2d1a10245998620d37054e9c9a7b9
bryer01 <97483747+bryer01@users.noreply.github.com>
2023-01-10 21:10:34 -0700

    changes to 3,4,5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,10 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {Random randomgenerator = new Random();
-      Console.WriteLine("Guess a Random Number"  );
+    {
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
-
-
 }
-
