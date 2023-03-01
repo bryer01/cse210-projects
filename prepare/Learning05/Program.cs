e9866621d3bd626a2586791dccd60d598608163a
bryer01 <97483747+bryer01@users.noreply.github.com>
2023-03-01 12:19:47 -0700

    Changes to Shapes.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Shape square = new Square("Red", 5);
+        Shape rectangle = new Rectangle("Blue", 3, 4);
+        Shape circle = new Circle("Green", 2);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: {0}, Area: {1}", shape.GetColor(), shape.GetArea());
+        }
+
+        Console.ReadLine();
     }
 }
