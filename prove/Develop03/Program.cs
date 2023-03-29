9a8342d7322c332cba99ecb4834da7fd2ffb09aa
bryer01 <97483747+bryer01@users.noreply.github.com>
2023-03-29 15:18:12 -0600

    new changes made to scripture assignment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,33 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Reference _ref = new Reference("John", 11, 35);
-        Scripture scripture = new Scripture(_ref, "Jesus Wept.");
-        Reference _ref2 = new Reference("1 Nephi", 2, 15);
-        Scripture scripture1 = new Scripture(_ref2, "And my father dwelt in a tent.");
-        Reference _ref3 = new Reference("John", 3, 16);
-        Scripture scripture2 = new Scripture(_ref3, "For God so loved the world, that he game his only begotten son, that whosever believeth in him should not perish, but have everlasting life.");
-        Reference _ref4 = new Reference("Matthew", 5, 3);
-        Scripture scripture3 = new Scripture(_ref4, "Blessed are the poor in spirit: for theirs is the kingdom of heaven.");
-        Reference _ref5 = new Reference("Proverbs", 3, 5 - 6);
-        Scripture scripture4 = new Scripture(_ref5, "Trust in the Lord with all thine heart; and lean not unto thine own understanding,\n In all thy ways acknowledge him, and he sall direct thy paths.\n");
+        // Create a new scripture object
+        Scripture scripture = new Scripture("Proverbs 3:5-6", "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight.");
 
-        while (true)
+        // Clear the console screen and display the complete scripture
+        Console.Clear();
+        scripture.Display();
+
+        // Prompt the user to press the enter key or type quit
+        Console.WriteLine("Press Enter to hide a word or type 'quit' to end the program.");
+        string input = Console.ReadLine();
+
+        // Hide random words until all words are hidden or the user types 'quit'
+        while (input != "quit" && scripture.HideRandomWord())
         {
-            Console.Write("Enter a command (or 'quit' to exit): ");
-            string input = Console.ReadLine();
+            // Clear the console screen and display the scripture again
+            Console.Clear();
+            scripture.Display();
 
-            if (input == "quit")
-            {
-                break;
-            }
-
-            // Handle the input command here
+            // Prompt the user to press the enter key or type quit
+            Console.WriteLine("Press Enter to hide another word or type 'quit' to end the program.");
+            input = Console.ReadLine();
         }
     }
-
-
 }
+
+
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,50 +1,58 @@
 using System;
 using System.Collections.Generic;
 
-
 class Scripture
 {
+    // Private fields
+    private string reference;
+    private string text;
+    private List<string> words;
 
-    private Reference _ref;
-    private List<Word> _words;
-
-    public Scripture(Reference _ref, string text)
+    // Constructor for a single verse
+    public Scripture(string reference, string text)
     {
-        this._ref = _ref;
-        _words = new List<Word>();
-        foreach (var word in text.Split(' '))
-        {
-            _words.Add(new Word(word));
-        }
+        this.reference = reference;
+        this.text = text;
+        this.words = new List<string>(text.Split(' '));
     }
 
-    public void HideWords(int count)
+    // Constructor for a verse range
+    public Scripture(string reference, int startVerse, int endVerse, string[] text)
     {
-        int i = 0;
-        while (i < _words.Count && count > 0)
-        {
-            _words[i].Hide();
-            i++;
-            count--;
-        }
+        this.reference = reference;
+        this.text = string.Join(" ", text, startVerse - 1, endVerse - startVerse + 1);
+
     }
 
-    public bool IsCompletelyHidden()
-
-    {
-        foreach (var word in _words)
-        {
-            //if (!word._isHidden)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
+    // Display the complete scripture, including the reference and the text
     public void Display()
     {
-        Console.WriteLine(_ref.GetDisplay());
-        Console.WriteLine(string.Join(" ", _words));
+        Console.WriteLine("{0}: {1}", reference, text);
+    }
+
+    // Hide a random word in the scripture
+    public bool HideRandomWord()
+    {
+        // Find the indices of all non-hidden words
+        List<int> nonHiddenIndices = new List<int>();
+        for (int i = 0; i < words.Count; i++)
+        {
+            if (!words[i].StartsWith("["))
+            {
+                nonHiddenIndices.Add(i);
+            }
+        }
+
+        // If there are no non-hidden words, return false
+        if (nonHiddenIndices.Count == 0)
+        {
+            return false;
+        }
+
+        // Select a random non-hidden word and replace it with a hidden version
+        int randomIndex = nonHiddenIndices[new Random().Next(nonHiddenIndices.Count)];
+        words[randomIndex] = "[" + words[randomIndex] + "]";
+
+        return true;
     }
 }
